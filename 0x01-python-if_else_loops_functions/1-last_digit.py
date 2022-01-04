76d26152c2deb67b7d228223a86136540e0943d1
Kaylee Dao <kayleed2@gmail.com>
2022-01-04 09:13:23 -0600

    Solidify yesterday

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+if number < 0:
+    last_digit = last_digit * -1
+str = "Last digit of"
+ls = "and is less than 6 and not 0"
+if last_digit > 5:
+    print("{} {} is {} and is greater than 5".format(str, number, last_digit))
+if last_digit == 0:
+    print("{} {} is {} and is 0".format(str, number, last_digit))
+if last_digit < 6 and last_digit != 0:
+    print("{} {} is {} {} ".format(str, number, last_digit, ls))
