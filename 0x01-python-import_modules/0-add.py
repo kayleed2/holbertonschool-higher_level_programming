3e89cbff74983945c4f320cd88b4e0034042b93d
Kaylee Dao <kayleed2@gmail.com>
2022-01-05 11:44:28 -0600

    if __name__

--- 0x01-python-import_modules/0-add.py
+++ 0x01-python-import_modules/0-add.py
@@ -2,5 +2,7 @@
 
 a = 1
 b = 2
-import add_0
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+from add_0 import add
+print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    import sys
