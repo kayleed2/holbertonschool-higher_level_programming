c1710f6b41779ea5f0eda804ffc48fbed9874afc
Kaylee Dao <kayleed2@gmail.com>
2022-01-05 12:43:21 -0600

    If one arg

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -5,6 +5,7 @@ if elements == 1:
     print("0 arguments.".format(elements))
 elif elements == 2:
     print("{} argument:".format(elements - 1))
+    print("{}: {}".format(elements - 1, sys.argv[elements - 1]))
 else:
     print("{} arguments:".format(elements - 1))
     for i in range(1, elements):
