72ed10c0e1bcfd73d673975e918d0a3d2c709b51
Kaylee Dao <kayleed2@gmail.com>
2022-01-05 12:28:56 -0600

    iterate through argv

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+elements = len(sys.argv)
+if elements == 1:
+    print("0 arguments.".format(elements))
+elif elements == 2:
+    print("{} argument:".format(elements - 1))
+else:
+    print("{} arguments:".format(elements - 1))
+    for i in range(1, elements):
+        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
