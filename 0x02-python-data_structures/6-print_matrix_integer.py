0019a4f817a0886bbd2f343627fb5506c2d0b678
Kaylee Dao <kayleed2@gmail.com>
2022-01-06 14:50:14 -0600

    No space after last value

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        print()
     for row in matrix:
         for ele in row:
-            if ele == row[-1]:
+            if ele is row[-1]:
                 print("{:d}".format(ele))
             else:
                  print("{:d}".format(ele), end=' ')
