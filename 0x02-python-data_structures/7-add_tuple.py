0b1c91c9e09d395fe4b01f84d4e83a764bdb634e
Kaylee Dao <kayleed2@gmail.com>
2022-01-06 18:28:44 -0600

    Too many if/elses

---
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 1:
+        a0 = tuple_a[0]
+        a1 = 0
+    elif len(tuple_a) == 0:
+        a0 = 0
+        a1 = 0
+    else:
+        a0 = tuple_a[0]
+        a1 = tuple_a[1]
+    if len(tuple_b) == 1:
+        b0 = tuple_b[0]
+        b1 = 0
+    elif len(tuple_b) == 0:
+        b0 = 0
+        b1 = 0
+    else:
+        b0 = tuple_b[0]
+        b1 = tuple_b[1]
+    tuple_sum = a0 + b0, a1 + b1
+    return tuple_sum
