7a72503f7ffb34b5a7676f928ca573d9165aebba
Kaylee Dao <kayleed2@gmail.com>
2022-01-11 16:17:06 -0600

    Print square

---
+++ 0x04-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Class Square - Defines squares based on 3-sq"""
+
+
+class Square:
+    """Class Square - Defines squares based on 3-sq"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print('#', end="")
+            print()
