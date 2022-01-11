8b6b340a452f7414201c3303474006a39667410f
Kaylee Dao <kayleed2@gmail.com>
2022-01-11 14:49:02 -0600

    README and 0

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square Class Description
+
+    Args:
+       __size (int): Size of square
+
+    """
+class Square:
+    def __init__(self, __size):
+        self.__size = __size
