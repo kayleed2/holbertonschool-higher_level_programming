9a35b6197d6e28c53af341e5728be592faf7679e
Kaylee Dao <kayleed2@gmail.com>
2022-01-11 14:55:25 -0600

    Documentation

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Square Class - Parameter Size"""
+""" Square Class - Parameter Size """
 class Square:
+    """ Square Class - Parameter Size """
     def __init__(self, __size):
         self.__size = __size
