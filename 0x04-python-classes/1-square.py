66b4424c243500284889dba88bbadb1ba1902d93
Kaylee Dao <kayleed2@gmail.com>
2022-01-11 15:26:12 -0600

    Pep

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -5,8 +5,8 @@
 class Square:
     """Class Square - Defines squares based on 0-sq"""
     def __init__(self, size=0):
-        if isinstance(size, int) == False:
-             raise TypeError("size must be an integer")
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
