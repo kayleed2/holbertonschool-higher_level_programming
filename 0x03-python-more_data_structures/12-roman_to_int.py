8eeb8b8757d18790c33b078271fe874bb00bf2ac
Kaylee Dao <kayleed2@gmail.com>
2022-01-10 19:48:53 -0600

    Can't figure out I before X

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,9 @@
 def roman_to_int(roman_string):
     if not roman_string or roman_string is None:
         return 0
-    roman = dict([('I', 1), ('V', 5), ('X', 10), ('L', 50), ('C', 100), ('D', 500), ('M', 1000)])
+    roman = dict([('I', 1), 
+    ('V', 5), ('X', 10), ('L', 50), 
+    ('C', 100), ('D', 500), ('M', 1000)])
     num = 0
     for i in roman_string:
         if i in roman:
