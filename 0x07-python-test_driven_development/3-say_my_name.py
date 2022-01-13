db120d503c954738fc1eba78b4cf5fb57161e642
Kaylee Dao <kayleed2@gmail.com>
2022-01-13 16:55:30 -0600

    pep and more tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,9 +10,9 @@ def say_my_name(first_name, last_name=""):
     """
     if isinstance(first_name, str) is False:
         raise TypeError('first_name must be a string')
-    
+
     elif isinstance(last_name, str) is False:
         raise TypeError('last_name must be a string')
-    
+
     else:
         print("My name is {} {}".format(first_name, last_name))
