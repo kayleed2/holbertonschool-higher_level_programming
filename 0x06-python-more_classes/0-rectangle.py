b87e8c2b53622de6c8fb78e577f3192cb0b20fb8
Kaylee Dao <kayleed2@gmail.com>
2022-01-14 12:13:23 -0600

    Accounting for invalid argument on instantiation

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -31,5 +31,16 @@ class Rectangle:
             self.__height = value
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        if isinstance(width, int) is False:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+        if isinstance(height, int) is False:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
