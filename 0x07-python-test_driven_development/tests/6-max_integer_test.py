8d4de4a31c87e33b50d8fb914c295dd822d1fe76
Kaylee Dao <kayleed2@gmail.com>
2022-01-16 17:29:33 -0600

    Partial file

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+    if __name__ == '__main__':
+        unittest.main()
