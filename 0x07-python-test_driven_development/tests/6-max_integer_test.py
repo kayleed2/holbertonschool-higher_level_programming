d57ed537a88cb5df73fdc838e2010d626304a8c5
Kaylee Dao <kayleed2@gmail.com>
2022-01-17 17:08:41 -0600

    A couple tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,6 +9,8 @@ class TestMaxInteger(unittest.TestCase):
     def test_ismax(self):
         self.assertEqual(max_integer([5, 12, 2, -6]), 12)
         self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([12, 1, 2, -6]), 12)
+        self.assertEqual(max_integer([5, 1, 2, 12]), 12)
     
     def test_isint(self):
         self.assertIsInstance(max_integer([5, 7, 3, -1]), int)
