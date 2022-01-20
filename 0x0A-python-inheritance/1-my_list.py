fcb6b9f34e6a38d7618f39e3cbeb1127c185a47c
Kaylee Dao <kayleed2@gmail.com>
2022-01-20 11:12:46 -0600

    No need to init

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Creation of class MyList that inherits from list:
+python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+class MyList(list):
+    """Class inheriting list:
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+
+    def print_sorted(self):
+        """Prints list sorted in ascending order:
+        python3 -c 'print(__import__("my_module").
+        MyClass.my_function.__doc__)'"""
+        print(sorted(self))
