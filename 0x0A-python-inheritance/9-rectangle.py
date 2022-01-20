3c6453a0ad7b61bb1e1e9b2ce64936c38848428c
Kaylee Dao <kayleed2@gmail.com>
2022-01-20 14:57:21 -0600

    Had to ensure validator was setting values when true

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module contains  a class Rectangle that inherits
+class BaseGeometry (based on 8-base_geometry.py):
+python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """This class is based on BaseGeometry from
+    the module 7-base_geometry.py:
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+    def __init__(self, width, height):
+        """This method instantiates with width and height:
+        python3 -c 'print(__import__("my_module").
+        MyClass.my_function.__doc__)'"""
+        if self.integer_validator("width", width):
+            self.__width = width
+        if self.integer_validator("height", height):
+            self.__height = height
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def area(self):
+        return self.__width * self.__height
