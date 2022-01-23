651ebd0ae1523696969d5e2ebc83512c5e22b4b8
Kaylee Dao <kayleed2@gmail.com>
2022-01-23 17:21:21 -0600

    pep8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,9 +2,6 @@
 """Module containing the function class_to_json"""
 
 
-import json
-
-
 def class_to_json(obj):
     """function that returns the dictionary
     description with simple data structure """
