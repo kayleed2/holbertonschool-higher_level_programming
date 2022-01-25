001312e0143ece7e0766c40619c07905aad28d68
Kaylee Dao <kayleed2@gmail.com>
2022-01-25 13:24:30 -0600

    All reqs and task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module with base class Base that will serve as base class for
+following projects"""
+
+
+class Base:
+    """This class is base of all other classes in "almost a circle project"""
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
