fd1cbc6245d19194585f1e2b191b9042c4d512e7
Kaylee Dao <kayleed2@gmail.com>
2022-01-27 12:07:45 -0600

    from_json_string as STATIC method. Updating dummy instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,8 +24,7 @@ class Base:
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
@@ -46,15 +45,17 @@ class Base:
                     with open(f'{type(el).__name__}.json', 'w') as f:
                         f.write(new_list)
 
-    @classmethod
+    @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
-        if json_string is None or json_string == []:
+        if json_string is None or json_string == "[]":
             new_list = []
             return new_list
-        else:
-            return json.loads(json_string)
+        return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
+        dummy = cls(1, 2, 3)
+        dummy.update(**dictionary)
+        return dummy
