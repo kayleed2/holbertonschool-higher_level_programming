d84a97cae61cec7fe1e0d54a4f35f2a2b05df8af
Kaylee Dao <3597@holbertonschool.com>
2022-02-10 19:36:15 -0800

    Ordering by cities.id

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- lists all cities
-SELECT cities.id, cities.name, states.name FROM cities WHERE cities.state_id
-= id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON
+cities.state_id = id ORDER BY cities.id ASC;
