5cb290a200a51cf1b45f6be01272f340793f170e
Kaylee Dao <3597@holbertonschool.com>
2022-02-10 19:48:13 -0800

    Left Join

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities
-SELECT cities.id, cities.name, states.name FROM cities
-LEFT JOIN states ON states.id = cities.state_id
+SELECT cities.id, cities.name, states.name FROM cities, states
+WHERE states.id = cities.state_id
 ORDER BY cities.id ASC;
