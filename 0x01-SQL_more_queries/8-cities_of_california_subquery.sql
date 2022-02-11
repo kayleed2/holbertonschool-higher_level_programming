58f10ada439eabe89dde354469d34641bb436d3a
Kaylee Dao <3597@holbertonschool.com>
2022-02-10 18:28:03 -0800

    Subquery

---
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,2 @@
+--  lists all the cities of California that can be found in the database
+SELECT id, name FROM cities WHERE state_id = (SELECT id from states WHERE name = 'California') ORDER BY ASC;
