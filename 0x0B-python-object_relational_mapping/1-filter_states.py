5397902aee24554a8401c6c6487d4064961913dc
Kaylee Dao <3597@holbertonschool.com>
2022-02-22 07:49:06 -0800

    SELECT * FROM states WHERE name LIKE

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import MySQLdb
+
+conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root",
+                       db="hbtn_0e_0_usa", charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+cur.close()
+conn.close()
