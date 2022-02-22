222f914c9865c8e2f0233e34488bc9ff10fb9112
Kaylee Dao <3597@holbertonschool.com>
2022-02-22 08:35:45 -0800

    Changing to sys.argv for user, passwd, db

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import MySQLdb
+import sys
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root",
-                       db="hbtn_0e_0_usa", charset="utf8")
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                       passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 cur = conn.cursor()
 cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
 rows = cur.fetchall()
