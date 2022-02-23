3dee8fe6cca1a9cbad80f1b108b3753883ab53df
Kaylee Dao <3597@holbertonschool.com>
2022-02-23 09:41:24 -0800

    name is main

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """  script that takes in an argument and displays all values in the states """
-import MySQLdb
-import sys
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                       passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-cur = conn.cursor()
+if __name__ == '__main__':
 
-cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY states.id ASC",
-            [format(sys.argv[4])])
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-conn.close()
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+
+    cur.execute("""SELECT * FROM states
+    WHERE name LIKE %s ORDER BY states.id ASC""", [format(sys.argv[4])])
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
