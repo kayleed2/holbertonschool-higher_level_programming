ee5dd34d643ee5573b89eca0a455323e482cce83
Kaylee Dao <3597@holbertonschool.com>
2022-02-23 11:43:21 -0800

    Tabbing so that code is within name == main

--- 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,7 +2,6 @@
 """ lists all state objects from database hbtn_0e_6_usa """
 
 if __name__ == '__main__':
-    import sqlalchemy
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
     from model_state import Base, State
@@ -10,12 +9,12 @@ if __name__ == '__main__':
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
+                                   sys.argv[3]))
+    Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
-for state in session.query(State).order_by(State.id).all():
-    print("{}: {}".format(state.id, state.name))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
 
-session.close()
+    session.close()
