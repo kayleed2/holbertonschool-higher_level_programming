9ff7d2e6b08e6efe072ab64396a3f788150dbbee
Kaylee Dao <3597@holbertonschool.com>
2022-02-23 09:44:04 -0800

    pycode

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,9 @@ if __name__ == '__main__':
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
 
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC")
+    cur.execute("""SELECT cities.id, cities.name, states.name
+    FROM cities
+    LEFT JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC""")
 
     rows = cur.fetchall()
     for row in rows:
