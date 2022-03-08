85306cee2d624afd6991f65fd51339afac60df49
Kaylee Dao <kayleed2@gmail.com>
2022-03-08 17:27:03 -0600

    Changing to > 1 argv

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
-    if len(sys.argv) == 3:
+    if len(sys.argv) > 1:
         q = sys.argv[1]
     else:
         q = ""
