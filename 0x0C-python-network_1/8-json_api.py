7a9d7e4cd9bb2f822bb286ed489487aabcc37aab
Kaylee Dao <kayleed2@gmail.com>
2022-03-08 17:30:04 -0600

    Checking for ints

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 1 and type(sys.argv[1]) != int:
         q = sys.argv[1]
     else:
         q = ""
