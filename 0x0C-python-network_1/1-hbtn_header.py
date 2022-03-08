fe79a08a79d62f88a179b4bcd899b575d0248e13
Kaylee Dao <kayleed2@gmail.com>
2022-03-08 11:32:12 -0600

    Changing it to sys

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -10,6 +10,7 @@ import sys
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
         html = response.read()
     print(response.headers['X-Request-Id'])
