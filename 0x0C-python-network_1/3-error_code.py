67d301b3abe1b8dc671e729d5f20cca44fdfa7a5
Kaylee Dao <kayleed2@gmail.com>
2022-03-08 17:58:24 -0600

    Cleaning up

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
         with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
     except HTTPError as e:
-            print("Error Code: {}".format(e.status_code))
+            print("Error Code: ", e.code)
