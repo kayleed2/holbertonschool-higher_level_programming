edd4dbe73e2347992d8bc5c769cffd273e6cf8df
Kaylee Dao <kayleed2@gmail.com>
2022-03-08 16:40:26 -0600

    Just upload files

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -6,5 +6,6 @@ import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-    print(html)
+        print("Body response:")
+        print('\t- type: {}'.format(type(response.text)))
+        print('\t- content: {}'.format(response.text))
