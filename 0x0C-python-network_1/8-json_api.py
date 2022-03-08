dc8dba58c8f8decfc2776b89a821152df834142a
Kaylee Dao <kayleed2@gmail.com>
2022-03-08 17:15:52 -0600

    Hopeful

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -3,11 +3,23 @@
 Python script that fetches https://intranet.hbtn.io/status
 """
 
+from json import JSONDecodeError
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) == 3:
+        q = sys.argv[2]
+    else:
+        q = ""
+    r = requests.post(url, data={q: q})
+    try:
+        res = r.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res['id'], res['name']))
+    except JSONDecodeError as er:
+        print("Not a valid JSON")
