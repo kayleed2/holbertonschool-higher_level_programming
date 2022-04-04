89270a2b40bc8a898216883ad8fd5bb17a631885
Kaylee Dao <kayleed2@gmail.com>
2022-04-04 14:10:28 -0500

    Array and for loop

---
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/opt/homebrew/bin/node
+// #!/usr/bin/node
+// Prints a message depending on passed arguments
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(myArray[i]);
+}
