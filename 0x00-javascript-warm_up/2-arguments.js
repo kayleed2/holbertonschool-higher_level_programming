0bea3cf0d46acd3a2e1ce396720844eec06aac50
Kaylee Dao <kayleed2@gmail.com>
2022-04-04 13:38:41 -0500

    semistandard

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -5,6 +5,8 @@ const number = process.argv.length;
 
 if (number <= 2) {
   console.log('No argument');
+} else if (number === 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
