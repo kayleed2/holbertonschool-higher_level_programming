550adb3a9b4d3548b794ddd6367890c30a44f298
Kaylee Dao <kayleed2@gmail.com>
2022-04-04 13:17:04 -0500

    Task 1, changing var

---
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Prints 3 lines
+
+let myVar = 'C is fun';
+console.log(myVar);
+myVar = 'Python is cool';
+console.log(myVar);
+myVar = 'JavaScript is amazing';
+console.log(myVar);
