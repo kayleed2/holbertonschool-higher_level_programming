67ba0480c8b6733d593a2cd3a54b05696b063bac
Kaylee Dao <kayleed2@gmail.com>
2022-04-07 12:48:39 -0500

    Adding tasks

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -5,8 +5,9 @@ const filePath = process.argv[2];
 const string = process.argv[3];
 const fs = require('fs');
 
-fs.writeFile(filePath, string, err => {
+fs.writeFile(filePath, string, (err, data) => {
   if (err) {
     console.error(err);
+    return;
   }
 });
