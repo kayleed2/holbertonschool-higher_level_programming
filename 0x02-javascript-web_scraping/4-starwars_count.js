25b180ac57fe9520d7bfc320ea03363252db4606
Kaylee Dao <kayleed2@gmail.com>
2022-04-08 09:30:21 -0500

    Access chars

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 // A script that display the status code of a GET request.
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
