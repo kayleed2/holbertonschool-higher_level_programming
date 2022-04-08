fcc0e3220126172cda4045213408cbc6426a6639
Kaylee Dao <kayleed2@gmail.com>
2022-04-08 09:36:53 -0500

    Checking newDict

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -1,13 +1,25 @@
-#!/usr/bin/node
+#!/opt/homebrew/bin/node
+// #!/usr/bin/node
 // A script that display the status code of a GET request.
 
 const request = require('request');
 const url = process.argv[2];
+const newDict = {};
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.parse(body));
+    const jsonContent = (JSON.parse(body));
+    for (let i = 0; i < jsonContent.length; i++) {
+      if (jsonContent[i].completed === true) {
+        if (jsonContent[i].userId in newDict) {
+          newDict[jsonContent[i].userId] += 1;
+        } else {
+          newDict[jsonContent[i].userId] = 1;
+        }
+      }
+    }
   }
+  console.log(newDict);
 });
