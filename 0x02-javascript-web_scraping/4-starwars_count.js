95d71a5b4cbd55e26c406356a8e8098a6087b611
Kaylee Dao <kayleed2@gmail.com>
2022-04-08 09:28:48 -0500

    Access chars

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,4 +1,5 @@
-#!/usr/bin/node
+#!/opt/homebrew/bin/node
+// #!/usr/bin/node
 // A script that display the status code of a GET request.
 
 const request = require('request');
@@ -8,10 +9,16 @@ request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const jsonContent = (JSON.parse(body));
+    const jsonContent = JSON.parse(body).results;
     let i = 0;
-    for (i in jsonContent.results) {
-      console.log(i);
+    for (const el in jsonContent) {
+      const chars = jsonContent[el].characters;
+      for (const x in chars) {
+        if (chars[x].includes('/18/')) {
+          i += 1;
+        }
+      }
     }
+    console.log(i);
   }
 });
